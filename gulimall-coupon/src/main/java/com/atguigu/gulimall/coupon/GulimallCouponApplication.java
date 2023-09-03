eb2a47ad1e23a8db933c5e8185868751505f8d5f
renyi <2632246528@qq.com>
2023-09-03 15:58:59 +0800

    添加网关、nacos

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -3,8 +3,11 @@ package com.atguigu.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.coupon.dao")
 public class GulimallCouponApplication {
 
